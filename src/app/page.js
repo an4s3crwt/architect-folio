808ac4ea3b148da6a937d11976fa4ac23f02ecd4
Ana Gilabert Lucas <anaymenta@gmail.com>
2024-08-15 20:41:21 +0200

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,191 @@
-import Image from "next/image";
+import Head from "next/head";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Head>
+        <title>Ana´s Secret</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta
+          name="description"
+          content="Working with web technologies ― I write software & design interfaces."
         />
-      </div>
+        
+        <link
+          rel="preload"
+          href="/fonts/1c0e60c1e41033b2-s.p.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/6eae7fac94fffd3d-s.p.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/873bdd61617580b9-s.p.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/fda11ca35e1f2c40-s.p.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://images.ctfassets.net/yry3oth0plee/Hnx3Az0sHwfgCsXWieMAq/2fb1ae93a4259b5c24b90a618cc6998f/R1-04558-011A.JPG?fm=jpg&q=85&w=1656"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://images.ctfassets.net/yry3oth0plee/1oUAYWG5EgUcHAj0sSS60Y/8462ec464b45a26eb37b286a1182a5e2/R1-07219-0023.JPG?fm=jpg&q=85&w=900"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://images.ctfassets.net/yry3oth0plee/784f96q8aCesyZRjP3cazY/633fff519374cb2875b618a95b68bf26/R1-07219-0008.JPG?fm=jpg&q=85&w=1656"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://res.cloudinary.com/dpznheznz/image/upload/w_1200,q_100/R1-08843-0009_yozeez.jpg"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://images.ctfassets.net/yry3oth0plee/JwvFe5bDcsNU2DgELPKEc/0d65a4e6fd9f60c6ad549a50841b603b/R1-00762-0019.JPG?fm=jpg&q=85&w=1656"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://images.ctfassets.net/yry3oth0plee/5Zc7sKmkKRSHpCd6g2g0Fr/c3c979cee3871d630b551455b7edae58/R1-07218-036A.JPG?fm=jpg&q=85&w=1656"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://res.cloudinary.com/dpznheznz/image/upload/w_1200,q_100/R1-04769-035A_dmq1pk.jpg"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://res.cloudinary.com/dpznheznz/image/upload/w_1200,q_100/R1-01536-031A_kw9sq4.jpg"
+        />
+        <link
+          rel="preload"
+          as="image"
+          href="https://images.ctfassets.net/yry3oth0plee/5hxxkR72mDONCMw2mEYtKM/e617cff8f75e1bad9ee351760db49931/R1-02919-014A.JPG?fm=jpg&q=85&w=1656"
+        />
+        <meta name="theme-color" content="#1b1b1b" />
+      </Head>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <main className="p-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-x-8 gap-y-4 leading-none
+         text-sm font-bold sticky top-4 z-20">
+          <div>⋢</div>
+          <a href="/" className="hover:underline" >
+            <h1>Ana´s Secret</h1>
+          </a>
+          <div>
+            Spain <sup>(b. 2003, ES)</sup>
+          </div>
+          <div>inbox@anasecret.com</div>
+        </div>
+        <div className="text-center mt-20 h-[300px] mb-[-120px]">
+          <div className="sticky top-[280px] md:top-[200px]">
+            <p className="text-xs">Developer at Arkeiftektbedr,</p>
+            <p className="opacity-50 text-xs">
+              Architects trade association in Spain.
+            </p>
+          </div>
+          <div className="polycarbonate flex mx-auto">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div class="flex flex-col w-full">
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/88/5d/fd/885dfd0584adf699df3ed1aef64c585c.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/11/dc/0b/11dc0b32ed7c009a91d3b7159ee346a6.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/4b/c3/a6/4bc3a69e125d01b732a8fe7249b6cf7b.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/76/f3/33/76f333a2c7400f56da209f66cd94dd45.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/736x/f9/6f/b4/f96fb419950754a2e5641fa210d5e567.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/c7/5b/83/c75b839ac4797dd644cc541b02aa06fd.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/ac/41/02/ac41025718046c72201f8d42f96a0645.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/f8/f5/14/f8f5145923065bd8268dc4bec94e5024.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+          <div class="flex flex-col h-[100vh] items-center justify-center">
+            <img src="https://i.pinimg.com/564x/f2/f4/b8/f2f4b8c88903a52d8c383bfd045d63a3.jpg" class="w-full max-w-xs z-10 mx-auto" />
+          </div>
+        </div>
+        <div className="fixed inset-0 z-[-1]">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 140">
+            <line x1="10" y1="20" x2="90" y2="50" stroke="black" stroke-width="0.7" />
+            <line x1="5" y1="5" x2="80" y2="100" stroke="black" stroke-width="0.03" />
+            <line x1="20" y1="0" x2="60" y2="120" stroke="black" stroke-width="0.1" />
+            <circle cx="10" cy="11" r="0.3" fill="black" />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <circle cx="58" cy="60" r="0.3" fill="black" />
+            <circle cx="84.1" cy="80.5" r="0.3" fill="black" />
+            <line x1="30" y1="10" x2="90" y2="70" stroke="black" stroke-width="0.03" />
+            <line x1="40" y1="90" x2="70" y2="40" stroke="black" stroke-width="0.03" />
+
+
+            <line x1="10" y1="40" x2="90" y2="100" stroke="black" stroke-width="0.03" />
+            <line x1="20" y1="70" x2="80" y2="10" stroke="black" stroke-width="0.1" />
+            <line x1="0" y1="30" x2="100" y2="90" stroke="black" stroke-width="0.1" />
+            <line x1="20" y1="50" x2="70" y2="120" stroke="black" stroke-width="0.03" />
+            <line x1="30" y1="20" x2="60" y2="100" stroke="black" stroke-width="0.03" />
+
+            <line x1="10" y1="30" x2="90" y2="80" stroke="black" stroke-width="0.1" stroke-dasharray="0.5, 1" />
+            <line x1="0" y1="50" x2="100" y2="120" stroke="black" stroke-width="0.05" stroke-dasharray="0.5, 1" />
+            <line x1="30" y1="10" x2="30" y2="90" stroke="black" stroke-width="0.1" />
+            <line x1="10" y1="70" x2="80" y2="70" stroke="black" stroke-width="0.1" />
+          </svg>
+        </div>
+
+
+
+      </main>
+    </>
+
   );
 }
